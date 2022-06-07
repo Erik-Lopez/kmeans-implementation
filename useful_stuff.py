f9a691adcf5140b3289e971a52489cde7caa75f5
Erik <eriklf442@gmail.com>
2022-06-07 21:19:13 +0200

    Add get_index_of_smallest_value() function

--- useful_stuff.py
+++ useful_stuff.py
@@ -1,2 +1,12 @@
 def cartesian_distance_between(p1, p2):
     return ((p1 - p2)**2)**0.5
+
+def get_index_of_smallest_value(array):
+    smallest_element = [0, float("inf")]
+
+    for idx,element in enumerate(array):
+        if element < smallest_element[1]:
+            smallest_element[0] = idx
+            smallest_element[1] = element
+    
+    return smallest_element[0] 
